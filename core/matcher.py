022a119fa8febee47ac01d1a0cf2ff641eca9b41
Nikola <neekolad@gmail.com>
2025-10-09 22:17:44 +0200

    added matching logic, make email body, subject, etc

--- core/matcher.py
+++ core/matcher.py
@@ -41,19 +41,59 @@ class Matcher:
             for outage_id, source, json_data in outages:
                 print(f"json_data: {json_data}\nkeywords: {keywords}")
                 if self.match_keywords(json_data, keywords):
-                    print("WE GOT A MATCH?")
+                    # print("WE GOT A MATCH")     # debug
 
-
-                # for kw in keywords.split(','):
-                #     kw_lower = kw.strip().lower()
-                #     if kw_lower in data.lower() or kw_lower in region.lower():
-                #         matched.append((user_id, outage_id))
-                #         # self.insert_email(user_id, outage_id, email)
-                #         print(user_id, outage_id)
-                #         break  # avoid duplicates for same outage/user
+                    # gather information needed, insert email into emails_queue, set outage processed 1
+                    # user_id, outage_id, email, phone, email_subject, email_body, status (pending)
+                    email_subject = "Komubot obavestenje"   # in the future maybe set APP_NAME + notification
+                    email_body = self.make_message(json_data, keywords.split(","))
+                    data = [user_id, outage_id, email, phone, email_subject, email_body, "pending"]
+                    print(json.dumps(data, indent=4))
+ 
+                    # insert into emails_queue
+                    # set outage processed
                 
-                # match those keywords and insert email to emails_queue table
         return matched
+    
+    def make_message(self, json, keywords):
+        today = datetime.now()
+        day = today.strftime("%d")
+        month = today.strftime("%B")
+        year = today.strftime("%Y")
+        the_time = today.strftime("%H:%M:%S")
+
+        body = self.msg_text(json, keywords)
+
+        message = f"""
+            <html>
+            <body>
+                <h1 style="color:gray;">{body}</h1>
+                <br>
+                <hr>
+                <p style="font-size:14px; color:gray;">Poslato: {day}. {month} {year} - {the_time}</p>
+            </body>
+            </html>
+            """
+        return message
+    
+    def msg_text(self, json_data, keywords):
+        haystack = json.dumps(json_data, ensure_ascii=False).lower()
+        # print(json_data)
+        print(keywords)
+        outage_regions = []
+        for lst in json.loads(json_data):
+            for item in lst['regions']:
+                dt = lst['date']
+                tm = lst['time']
+                # print(item['region'])
+                for kw in keywords:
+                    # print(f"now checking {item['region'].lower()} with {kw.lower()}")     # debug
+                    if item['region'].lower() == kw.lower().strip():
+                        print(item['region'], 'found keyword', kw)
+                        outage_regions.append(f"{item['region']} - {', '.join([i for i in item['address']])}")
+        msg = f"Hey, there will be a water outage in your region on {dt} ({tm}) :\n{', '.join(outage_regions)}"
+        # print(msg)    # debug
+        return msg
     
     def insert_email(self, user_id, outage_id, email):
         with self.connect() as conn:
