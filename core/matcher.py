fed8fbd41105c97d54c845271891d321c847809a
Nikola <neekolad@gmail.com>
2025-10-09 22:59:44 +0200

    added insert email into emails_queue, set outage processed

--- core/matcher.py
+++ core/matcher.py
@@ -37,9 +37,9 @@ class Matcher:
         print(f"outages: {json.dumps(outages, indent=4)}")
         print(f"users: {json.dumps(users, indent=4)}")
         matched = []
-        for user_id, name, email, phone, keywords in users:
-            for outage_id, source, json_data in outages:
-                print(f"json_data: {json_data}\nkeywords: {keywords}")
+        for outage_id, source, json_data in outages:
+            for user_id, name, email, phone, keywords in users:
+                # print(f"json_data: {json_data}\nkeywords: {keywords}")    #debug
                 if self.match_keywords(json_data, keywords):
                     # print("WE GOT A MATCH")     # debug
 
@@ -47,14 +47,49 @@ class Matcher:
                     # user_id, outage_id, email, phone, email_subject, email_body, status (pending)
                     email_subject = "Komubot obavestenje"   # in the future maybe set APP_NAME + notification
                     email_body = self.make_message(json_data, keywords.split(","))
-                    data = [user_id, outage_id, email, phone, email_subject, email_body, "pending"]
-                    print(json.dumps(data, indent=4))
- 
+                    # data = [user_id, outage_id, email, phone, email_subject, email_body, "pending"]
+                    # print(json.dumps(data, indent=4))   # debug
+
                     # insert into emails_queue
-                    # set outage processed
-                
+                    email_queue = self.insert_email(user_id, outage_id, email, phone, email_subject, email_body, status="pending")
+                    if email_queue:
+                        print(f"Inserted mail for user_id {user_id} outage_id {outage_id}")
+                        matched.append((user_id, outage_id))
+
+            # set outage processed
+            outage_processed = self.set_outage_processed(outage_id)
+            if outage_processed:
+                print(f"Succesfully set processed=1 user_id for outage_id {outage_id}")
         return matched
     
+
+    def insert_email(self, user_id, outage_id, email, phone, email_subject, email_body, status):
+        try:
+            with self.connect() as conn:
+                conn.execute("""
+                    INSERT INTO emails_queue (user_id, outage_id, email_addr, phone, email_subject, email_body, status)
+                    VALUES (?, ?, ?, ?, ?, ?, ?)
+                """, (user_id, outage_id, email, phone, email_subject, email_body, status))
+                conn.commit()
+            return True
+        except Exception as e:
+            print(f"Failed to insert record into emails_queue (user_id {user_id}, outage_id {outage_id}): {e}")
+            return False
+        
+
+    def set_outage_processed(self, outage_id):
+        print("OUTAGE ID:", outage_id)
+        try:
+            with self.connect() as conn:
+                conn.execute("""
+                    UPDATE outages SET processed=1 WHERE id=?
+                """, (outage_id,))
+            return True
+        except Exception as e:
+            print(f"Failed to set outage processed=1 for outage_id {outage_id}: {e}")
+            return False
+
+
     def make_message(self, json, keywords):
         today = datetime.now()
         day = today.strftime("%d")
@@ -76,6 +111,7 @@ class Matcher:
             """
         return message
     
+    
     def msg_text(self, json_data, keywords):
         haystack = json.dumps(json_data, ensure_ascii=False).lower()
         # print(json_data)
@@ -95,10 +131,3 @@ class Matcher:
         # print(msg)    # debug
         return msg
     
-    def insert_email(self, user_id, outage_id, email):
-        with self.connect() as conn:
-            conn.execute("""
-                INSERT OR IGNORE INTO email_queue (user_id, outage_id, email, status, created_at)
-                VALUES (?, ?, ?, 'pending', ?)
-            """, (user_id, outage_id, email, datetime.now().isoformat()))
-            conn.commit()
